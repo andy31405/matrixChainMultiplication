2ebfcbfd036601ed9c1f17183cec6b7934bc2026
andy31405 <50476999+andy31405@users.noreply.github.com>
2020-01-01 21:49:01 +0800

    Add files via upload

---
+++ matrix_chain _multiplication.h
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int N;
+int s[32][32];
+void print(int i, int j){
+	if(i==j){
+		cout<<"A"<<i;
+	}else{
+		cout<<"(";
+		print(i, s[i][j]);
+		print(s[i][j]+1, j);
+		cout<<")";
+	}
+}
+void order(int *p, int n){
+	N=n;
+	int m[n+1][n+1], j, q;
+	for(int i=0;i<=n;i++){
+		m[i][i]=0;
+	}
+	for(int l=2;l<=n;l++){
+		for(int i=1;i<=n-l+1;i++){
+			j=i+l-1;
+			m[i][j]=INT_MAX;
+			for(int k=i;k<j;k++){
+				q=m[i][k]+m[k+1][j]+p[i-1]*p[k]*p[j];
+				if(q<m[i][j]){
+					m[i][j]=q;
+					s[i][j]=k;
+				}
+			}
+		}
+	}
+	print(1, n);
+}
