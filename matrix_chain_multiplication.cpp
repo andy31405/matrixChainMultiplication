1433deb518173474f4963541d5fc9654c0db82b2
andy31405 <50476999+andy31405@users.noreply.github.com>
2020-01-01 21:55:32 +0800

    Add files via upload

---
+++ matrix_chain_multiplication.cpp
@@ -0,0 +1,13 @@
+#include "matrix_chain_multiplication.h"
+int main(){
+	int n;
+	cout<<"Please enter the number of matrix: ";
+	cin>>n;
+	int p[n+1];
+	cout<<"Please enter the chain("<<n+1<<"): ";
+	for(int i=0;i<=n;i++){
+		cin>>p[i];
+	}
+	order(p, n);
+	return 0;
+}
